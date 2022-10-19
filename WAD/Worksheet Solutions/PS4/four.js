7e127b859c9eb8c27d5dca521694164d77294505
22mx108 <22mx108@MCA.PSG>
2022-10-19 16:24:00 +0530

    js

--- WAD/Worksheet Solutions/PS4/four.js
+++ WAD/Worksheet Solutions/PS4/four.js
@@ -5,7 +5,7 @@ for(i=0;i<5;i++)
 {
     for(j=0;j<i;j++) {
     if(j%2===0)
-        process.stdout.write(odd)
+        process.stdout.write(odd) //javascript method
     else
     process.stdout.write(even)
         
