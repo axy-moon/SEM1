37f7aa6acc771d7e891ba365cbbaceeeabb756ee
axy-moon <22mx108@MCA.PSG>
2022-10-31 16:09:00 +0530

    DS

--- Data Structures/PS4/six.c
+++ Data Structures/PS4/six.c
@@ -3,19 +3,34 @@
 int main() {
 
     int numArray[] = {1,2,3,4,5,6};
+    int len;
 
-    int key=2;
+    len = sizeof(numArray)/sizeof(numArray[0]);
+    int key;
 
-    for(int i=0;i<5;i++){
-
-            
-            numArray[i] = numArray[i+2];
-
-        }
-
-    for(int j = 0;j<5;j++) 
-        printf("%d ",numArray[j]);
+    printf("Enter the value of key: ");
+    scanf("%d",&key);
     
-    }
+    int j;
+    int temp;
+
+      for (int i=0; i<key; i++) {   
+      
+        j=len-1;                    
+        temp = numArray[j];         
+      
+      for (; j>0; j--) {
+         numArray[j] = numArray[j-1];    
+      }
+      
+      numArray[0] = temp;
+   }
+
+    for(int c = 0;c<len;c++) 
+        printf("%d ",numArray[c]); 
+    
+}
+
+    
 
 
