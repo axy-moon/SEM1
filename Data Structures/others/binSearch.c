fc731ff7257cb20c98820acab7c4ed42b762a503
axy-moon <22mx108@MCA.PSG>
2022-11-02 18:23:35 +0530

    02/Nov

--- Data Structures/others/binSearch.c
+++ Data Structures/others/binSearch.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
-int main() {
+void main() {
 
-    int numArray[] = {1,2,3,4,5,7,8,9,11,14,16,18,20};
-    int low = 0;
-    int high = sizeof(numArray)/sizeof(numArray[0]);
-    int mid = (low + high)/2;
+    int numArray[] = {1,2,3,4,5,7,8,9,11,14,16,18,20}; //decleration
+    int low = 0;  // low = 0
+    int high = sizeof(numArray)/sizeof(numArray[0]); //len = 13
+    int mid = (low + high)/2;  //6
+   
     int value;
 
 
@@ -17,7 +18,7 @@ int main() {
         
         mid = (low+high)/2;
 
-        if(numArray[mid]>value)
+        if(numArray[mid]>value)  //mid value 
             high = mid - 1; 
         else if(numArray[mid]<value) 
             low = mid+1;
@@ -25,9 +26,7 @@ int main() {
             printf("%d is found in position %d", value,mid+1);
             return mid;
         }
+
     }
-
-    return 0;
-
 
 }
