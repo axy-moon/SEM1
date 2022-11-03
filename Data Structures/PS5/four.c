e0a86d265a997ee333d01fdecd1e6e734dfb386c
axy-moon <22mx108@MCA.PSG>
2022-11-03 12:06:19 +0530

    testc

--- Data Structures/PS5/four.c
+++ Data Structures/PS5/four.c
@@ -5,7 +5,7 @@
 int main() {
 
     char strOne[25];
-    char strOne[25];
+    char strTwo[25];
     int len;
     int j = 0;
     printf("Enter the string: ");
@@ -17,20 +17,28 @@ int main() {
     for(int i=0;i<len;i++) {
 
 
-        if(strOne[i]!='b')
-            strTwo[j] = strOne[i];
-        else if(strOne[i] == 'a'){
-            if(strOne[i+1] == 'c') {
-                strOne[i] = '';
-                strOne[i+1] = '';
-            }
+        if(strOne[i]!='b') {
+            if(strOne[i] != 'a' && strOne[i+1] != 'c') 
+                strTwo[j] = strOne[i];
+            else
+                j+2;
+        
+            
         }
-        j++;
+        else {
+            j++;
+            continue; }
+        /* else if(strOne[i] == 'a'){
+            if(strOne[i+1] == 'c') {
+               //
+               //
+            } */
+        
+        
 
     }
 
-    printf("%s",strOne);
-
+    printf("%s",strTwo);
 
     return 0;
 }
